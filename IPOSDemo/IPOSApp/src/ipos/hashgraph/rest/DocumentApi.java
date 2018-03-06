0a477419bd3d1ceba991bd41f654a375356b3727
minisha <mmurugan@palo-it.com>
2018-03-06 15:11:35 +0800

    Fix for Servlet config error

--- IPOSDemo/IPOSApp/src/ipos/hashgraph/rest/DocumentApi.java
+++ IPOSDemo/IPOSApp/src/ipos/hashgraph/rest/DocumentApi.java
@@ -1,70 +1,35 @@
 package ipos.hashgraph.rest;
 
-import com.txmq.exo.core.ExoPlatformLocator;
-import com.txmq.exo.messaging.ExoMessage;
 import io.swagger.annotations.Api;
-import ipos.hashgraph.IPOSAppState;
 import ipos.hashgraph.model.Document;
-import ipos.hashgraph.model.Documents;
-import ipos.hashgraph.transaction.TransactionType;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.io.IOException;
 
 @Path("/Hashgraph/1.0.0")
-@Api(value = "/Hashgraph/1.0.0", description = "Simple CRUD functions on Customer table")
-public class DocumentApi {
+@Api(value = "/Hashgraph/1.0.0", description = "Documents endpoints")
+public interface DocumentApi {
 
-	@GET
-	@Path("/endpoints")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response getEndpoints() {
-		IPOSAppState state = (IPOSAppState) ExoPlatformLocator.getPlatform().getState();
-		return Response.ok().entity(state.getEndpoints()).build();
-	}
+    @GET
+    @Path("/endpoints")
+    @Produces(MediaType.APPLICATION_JSON)
+    Response getEndpoints();
 
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
     @Path("/state")
-    public Response getState() {
-        IPOSAppState state = (IPOSAppState) ExoPlatformLocator.getPlatform().getState();
-        return Response.ok().entity(state).build();
-    }
+    Response getState();
 
+    @GET
+    @Path("/documents")
+    @Produces(MediaType.APPLICATION_JSON)
+    Response getDocuments();
 
-	@GET
-	@Path("/documents")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response getDocuments() {
-        IPOSAppState state = (IPOSAppState) ExoPlatformLocator.getPlatform().getState();
-		Documents result = new Documents();
-		if(state.getDocuments().size() == 0) {
-		    return Response.ok().entity(result).build();
-        }
-
-		result.setHash(state.getDocuments());
-		return Response.ok().entity(result).build();
-	}
-
-	@POST
-	@Path("/document")
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response addDocument(Document document) {
-		ExoMessage message = new ExoMessage(new TransactionType(TransactionType.ADD_DOC), document);
-        boolean transactionStatus = false;
-		try {
-		    transactionStatus = ExoPlatformLocator.getPlatform().createTransaction(message.serialize(), null);
-        } catch (IllegalStateException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return Response.status(201).entity("{\n" +
-                "  \"result\":"+transactionStatus+"\n" +
-                "}").build();
-	}
+    @POST
+    @Path("/document")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    Response addDocument(Document document);
 
 }
